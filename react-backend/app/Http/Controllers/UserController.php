559f75203b9f236d4427c7c517f7607f2c62961c
alumno <alumno@vm1>
2022-05-25 18:53:33 +0200

    pequeña mejora en register

--- react-backend/app/Http/Controllers/UserController.php
+++ react-backend/app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
