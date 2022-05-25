0d985d650cc5ff561d6244afde951dcabf34ddf7
alumno <alumno@vm1>
2022-05-25 18:57:20 +0200

    mejora en user model

--- react-backend/app/Models/User.php
+++ react-backend/app/Models/User.php
@@ -44,9 +44,9 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
+    /*protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+    ];*/
 
     public function esAdministrador()
     {
