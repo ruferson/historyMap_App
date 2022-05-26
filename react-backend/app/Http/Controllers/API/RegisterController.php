021afdc193783ec4e5e84b7e8cdadb6692e50f82
alumno <alumno@vm1>
2022-05-26 20:20:06 +0200

    Contraseñas en register funcionan

--- react-backend/app/Http/Controllers/API/RegisterController.php
+++ react-backend/app/Http/Controllers/API/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => bcrypt($request->password),
         ]);
 
         return response()->json(['status' => true, 'user' => $user]);
