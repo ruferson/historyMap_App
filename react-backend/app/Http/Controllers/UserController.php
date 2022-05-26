64be0633b0d3f3f8b2e24bd2bdc53ce9c05f9c01
Juan David Massanet Puentes <94039846+JuanDavid102@users.noreply.github.com>
2022-05-26 20:23:32 +0200

    Update UserController.php

--- react-backend/app/Http/Controllers/UserController.php
+++ react-backend/app/Http/Controllers/UserController.php
@@ -82,7 +82,7 @@ class UserController extends Controller
         $userDataArray = array(
             "name" => $request->name,
             "email" => $request->email,
-            "password" => bcrypt($request->password),
+            "password" => md5($request->password),
         );
 
         $user_status = User::where("email", $request->email)->first();
